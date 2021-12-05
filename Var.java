ba6d0451116a7010b2f4451cebfd6f687cae73fd
Kuldeep Hajare <hajarekuldeep7@gmail.com>
2021-12-05 22:29:17 +0530

    commit

---
+++ Var.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class Var
+{
+      public static void main(String arg[])
+      {
+            var no=13;
+
+            var initial ='K';
+
+            var name="Kuldeep";
+
+
+            System.out.println(no);
+            System.out.println(initial);
+            System.out.println(name);
+      }
+}
