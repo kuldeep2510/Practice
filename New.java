55dd4994ac1864ead88e2007cc86c2e0ad1dc866
Kuldeep Hajare <hajarekuldeep7@gmail.com>
2021-12-17 20:32:09 +0530

    commit

---
+++ New.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class New
+{
+      public static void main(String args[])
+      {
+            Integer pp,qq,rr;
+
+            Set pp=3,qq=5,rr=10;
+
+            for(int rr :)
+      }
+}
