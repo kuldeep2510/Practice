cb55799b0ba37b498a001b887504982e6793fc03
Kuldeep Hajare <hajarekuldeep7@gmail.com>
2022-02-19 11:01:42 +0530

    commit

--- Overriding.java
+++ Overriding.java
@@ -4,11 +4,39 @@ class Overriding
 {
       public static void main(String arg[])
       {
+            Demo dobj=new Demo();
+            dobj.fun();
+            dobj.gun();
+
+            Kemo kobj=new Kemo();
+            kobj.fun();
+            kobj.sun();
 
       }
 }
 
 class Demo
 {
-      
+      public void fun()
+      {
+            System.out.println("fun of Demo");
+      }
+
+      public void gun()
+      {
+            System.out.println("gun of demo");
+      }
+}
+
+class Kemo extends Demo
+{
+      public void sun()
+      {
+            System.out.println("sun of kemo");
+      }
+      @Override
+      public void fun()
+      {
+            System.out.println("fun of kemo");
+      }
 }
